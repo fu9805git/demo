246ec42d4f9a9c5ad3abde8eea1f3f0f92c047fe
SongYang513 <32841050+SongYang513@users.noreply.github.com>
2019-03-01 17:06:55 +0800

    删除多余文件

--- demo/src/main/java/com/example/demo/mapper/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.mapper;
-
-import com.example.demo.pojo.Users;
-
-public interface UserMapper {
-	void insertUser(Users users);
-}
--- demo/src/main/java/com/example/demo/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.pojo.Users;
-
-public interface UserService {
-	void addUser(Users users);
-}
